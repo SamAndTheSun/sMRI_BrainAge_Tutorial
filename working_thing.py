83b239a2c1117af3b27f54233cff374a2573e040
Sam Anderson <167593161+SamAndTheSun@users.noreply.github.com>
2024-09-10 15:44:07 -0700

    Create working_thing.py

---
+++ working_thing.py
@@ -0,0 +1,19 @@
+#Steps:
+
+#
+
+#set pathss
+INPUT_PATH = '/path_to_your_dicom_directory'
+OUTPUT_PATH = '/path_to_your_output_directory'
+
+#convert dicom files to nii or nii.gz format
+dcm2niix '$INPUT_PATH' --OutDirMode=1 --OutDir='$OUTPUT_PATH'> log_file.txt
+
+#select all of the files you want to run conversion on
+RAW_FILES = $(find '$OUTPUT_PATH' -type f \(-name '*.nii' -o '*.nii.gz'\))
+
+#run parallel processing of nii recons
+parallel --jobs 4 'recon-all -i {} -all ::: $RAW_FILES
+
+
+#next, add a subjects directory for freesurfer
