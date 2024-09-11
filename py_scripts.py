3573b01cdb4a60a69b5d9d59ff10a764ee2bdca2
Sam Anderson <167593161+SamAndTheSun@users.noreply.github.com>
2024-09-11 16:31:20 -0700

    Create py_scripts.py

---
+++ py_scripts.py
@@ -0,0 +1,27 @@
+import glob
+import dcmread
+
+def get_metadata(raw_dir, folder_pattern):
+
+    #get all folders containing dcm files
+    folders = glob.glob(raw_dir + folder_pattern)
+
+    metadata = []
+
+    for folder in folders:
+        dcm_files = glob.glob(folder + '*.dcm')
+        ds = dcmread(dcm_files[0]) #metadata should be identical for all dicoms in the same subject/timepoint folder
+        
+        #get all of the metadata from the dictionary
+        file_metadata = {}
+        for e in ds.iterall():
+            tag_name = e.name
+            file_metadata[tag_name] = e.value
+
+        #add the metadata for the file
+        metadata.append(file_metadata)
+
+    return folders #useful in determining sort order for later analysis
+    
+
+
