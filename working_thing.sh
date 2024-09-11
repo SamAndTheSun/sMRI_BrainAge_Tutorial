ff95df67245a9f38c0c1721b0e2ef1bcabb50efa
Sam Anderson <167593161+SamAndTheSun@users.noreply.github.com>
2024-09-11 14:53:18 -0700

    Update working_thing.sh

--- working_thing.sh
+++ working_thing.sh
@@ -1,11 +1,20 @@
 : '
-Steps:
+Steps for analyzing mri data:
 
+  Get metadata from dicom files
+  Convert dicom files to nii and niigz
+  Recon nii and niigz
+  Convert brain.mgz file, as ouputted by freesurfer, to NPY format
+  Format and apply neural network to NPY file 
+  Generate 4d saliency map showing neural network weighting across subjects
+  Compress this into 2d maps for further analysis
 '
 
 #set paths
 INPUT_PATH = '/path_to_your_dicom_directory'
 OUTPUT_PATH = '/path_to_your_output_directory'
+
+#get metadata from dicom files
 
 #convert dicom files to nii or nii.gz format
 dcm2niix '$INPUT_PATH' --OutDirMode=1 --OutDir='$OUTPUT_PATH'> log_file.txt
@@ -20,8 +29,4 @@ mkdir -p "$RECON_DIR"
 #run parallel processing of nii recons
 parallel --jobs 4 'recon-all -i {} -sd '"$RECON_PATH"' -all' ::: $RAW_FILES
 
-
-
-
-
-#next, add a subjects directory for freesurfer
+#identify all brain.mgz files, and convert these to a single NumPy array
