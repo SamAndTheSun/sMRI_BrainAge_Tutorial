a824d0883bf7e3b752d48d461f1b24676aa582cf
Sam Anderson <167593161+SamAndTheSun@users.noreply.github.com>
2024-09-11 16:55:40 -0700

    Update py_scripts.py

--- py_scripts.py
+++ py_scripts.py
@@ -1,6 +1,10 @@
+import os
 import glob
 import dcmread
+
+import numpy as np
 import pandas as pd
+import nibabel as nib
 
 def get_metadata(input_dir, out_dir, folder_pattern = '/*/.dcm'):
 
@@ -25,6 +29,20 @@ def get_metadata(input_dir, out_dir, folder_pattern = '/*/.dcm'):
     metadata_df = pd.DataFrame(metadata)
     metadata_df['File Path'] = folders
     metadata.to_excel(out_dir + 'metadata.xlsx')
+    
+    return
+
+def mgz_to_np(file_paths):
+
+    array = []
+
+    for path in file_paths:
+        img = nib.load(path)
+        data = img.get_fdata() #convert data to numPy array
+        array.append(data)
+
+    np_array = np.stack(array, axis=0)
+    np.save(out_dir + 'metadata.xlsx', np_array)
     
     return
     
