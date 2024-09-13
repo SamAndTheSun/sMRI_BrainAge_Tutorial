6baa780526decde6efefc3a9341b93107ca09f5d
Sam Anderson <167593161+SamAndTheSun@users.noreply.github.com>
2024-09-13 15:53:10 -0700

    Update working_thing.sh

--- working_thing.sh
+++ working_thing.sh
@@ -1,4 +1,8 @@
 : "
+Data:
+
+  Use https://openfmri.org/dataset/ds000002/ , it is a classification dataset. Train a CNN to learn off of this data for classification using categorical cross entropy, plot an AUC representing the accuracy
+
 Steps for analyzing mri data:
 
   Get metadata from dicom files
